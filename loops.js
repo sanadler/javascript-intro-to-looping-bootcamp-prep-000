355b519e7edab26b0399754e66ff0b767c7679d8
Sarah Nadler <sarah.m.nadler@gmail.com>
2017-11-09 21:10:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array){
   for(i=1; i<26; i++){
     if (i===1){
-      array[i] = "I am 1 strange loop"
+      array[0] = "I am 1 strange loop."
     }
     else{
-      array[i] = "I am" + i + "strange loops"
+      array[i] = "I am" + i + "strange loops."
     }
   }
   return array;
